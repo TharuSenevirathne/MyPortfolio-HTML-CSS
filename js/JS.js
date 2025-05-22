12bebe74e3196f8d4185bef52a92aed9415e6c17
TharuSenevirathne <tharusanduni33@gmail.com>
2025-05-22 01:58:59 -0700

    init typing animation is done

--- js/JS.js
+++ js/JS.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     // Initialize the typing animation
     const typingTextElement = document.querySelector('.typing-text');
     const textArray = ['Web Developer', 'UI/UX Engineer'];
@@ -7,6 +6,33 @@ document.addEventListener('DOMContentLoaded', function() {
     let charIndex = 0;
     let isDeleting = false;
     let typingDelay = 200;
+
+    function typeText() {
+        const currentText = textArray[textIndex];
+
+        if (isDeleting) {
+            typingTextElement.textContent = currentText.substring(0, charIndex - 1);
+            charIndex--;
+            typingDelay = 100;
+        } else {
+            typingTextElement.textContent = currentText.substring(0, charIndex + 1);
+            charIndex++;
+            typingDelay = 200;
+        }
+
+        if (!isDeleting && charIndex === currentText.length) {
+            isDeleting = true;
+            typingDelay = 1000;
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            textIndex = (textIndex + 1) % textArray.length;
+            typingDelay = 500;
+        }
+
+        setTimeout(typeText, typingDelay);
+    }
+
+    typeText();
 
 
 
