0266d8947fc3b31e8473238a2d66b5179c49f515
TharuSenevirathne <tharusanduni33@gmail.com>
2025-05-22 01:58:31 -0700

    init typing animation

---
+++ js/JS.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Initialize the typing animation
+    const typingTextElement = document.querySelector('.typing-text');
+    const textArray = ['Web Developer', 'UI/UX Engineer'];
+    let textIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    let typingDelay = 200;
+
+
+
+});
